e222dbab657a79adac36125bd3ce504572082fc9
shruti <shrutishevale1@gmail.com>
2025-03-12 13:04:50 +0530

    evenodd

---
+++ third.py
@@ -0,0 +1,11 @@
+# Python program to check if a number is even or odd
+
+# Ask the user for a number
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
