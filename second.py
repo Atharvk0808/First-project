d960dc3c2ca4de74d6afb0b17ac8fc89a5e3f30d
shruti <shrutishevale1@gmail.com>
2025-03-12 12:29:45 +0530

    cc

---
+++ second.py
@@ -0,0 +1,10 @@
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+
+next_year_age = age + 1
+
+
+print(f"Hello, {name}! Next year, you will be {next_year_age} years old.")
+
